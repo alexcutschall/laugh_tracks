5de9f0b9f41a102ed0027e441bfd4d66b64bdddc
alexcutschall <alexander.cutschall@gmail.com>
2018-03-16 10:43:59 -0600

    The seed will only work but the tests are in place

--- spec/features/user_visits_the_comedians_page_spec.rb
+++ spec/features/user_visits_the_comedians_page_spec.rb
@@ -1,12 +1,17 @@
 RSpec.describe Comedian do
   describe "user visits the comedians page" do
     context "user wants to see list of comedians special" do
+      it "should see list of comedians" do
+        visit '/comedians'
+
+        expect(page).to have_content("Hannibal")
+      end
       it "should see list of specials" do
         visit "/comedians"
 
-        within 'Hannibal Burress' do
+        within_fieldset ('Hannibal Burress') do
         click 'specials'
-        expect(page).to_have content("My Name is Hannibal")
+        expect(page).to have_content("My Name is Hannibal")
         end
       end
     end
